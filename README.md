c881b48fcd6254d205496ecca6219a7c71ff00a4
derek-corcoran-barrios <derek.corcoran.barrios@gmailcom>
2020-08-31 20:52:43 -0400

    Responses ready

--- README.md
+++ README.md
@@ -3,11 +3,17 @@
 
   - **Possible\_Sol.R:** Simulation of protein data to get the needed
     correlations.
+  - **Response\_Curves.R:** Building the response curves
 
 ## Files
 
+  - **avgmod.95p2.rds:** Average GLM model with cumsum of 0.95 akaike
+    weights.
+  - **df2\_corrected\_prot\_up\_low\_bound\_small.rds:** raw climatic
+    data for each site.
+  - **DF\_Prop\_gbm\_Est.rds:** Models for GBM prediction
   - **sars-cov2\_genomes\_clade\_region\_v3:** raw data
   - **Simulation\_response\_probs.rds:** 5000 simulated points generated
     from *Possible\_Sol.R*
-  - **avgmod.95p2.rds:** Average GLM model with cumsum of 0.95 akaike
-    weights. \* \*\*\*\*
+
+-----
--- Response_Curves.R
+++ Response_Curves.R
@@ -29,7 +29,8 @@ df2 <- df2 %>%
   dplyr::select(Protein, Proportion, Pred) %>% 
   group_by(Protein, Proportion) %>% 
   summarise_at("Pred", .funs = list(SD = sd, Lambda = mean)) %>% 
-  dplyr::filter(!is.na(SD))
+  dplyr::filter(!is.na(SD)) %>% 
+  mutate(Model = "GLM")
 
 ## Graph for glm
 ## Noisy dynamics
@@ -43,3 +44,95 @@ ggplot(df2, aes(x = Proportion, y = Lambda)) +
 ####GBM##################################################
 ##########################################################
 
+df3 <- df
+
+DF <-  read_rds("DF_Prop_gbm_Est.rds") %>% 
+  mutate(ID = 1:n(),Weight = 1/(R2 - R2_Post)^3, Total = sum(Weight), Std_Weight = Weight/Total)
+
+Models <- read_rds("models_Prop_gbm_Est.rds") 
+
+df3$Lambda<- Models %>% 
+  purrr::map(~predict(.x, df3)) %>% 
+  purrr::map2(.y = DF$Std_Weight,~.x*.y) %>% 
+  reduce(`+`)
+
+df3 <-  df3 %>% 
+  pivot_longer(starts_with("Est"), names_to = "Protein", values_to = "Proportion") %>% 
+  mutate(Protein = str_remove_all(Protein, "Est_"))  %>% 
+  dplyr::select(Protein, Proportion, Lambda) %>% 
+  group_by(Protein, Proportion) %>% 
+  summarise_at("Lambda", .funs = list(SD = sd, Lambda = mean)) %>% 
+  dplyr::filter(!is.na(SD)) %>% 
+  mutate(Model = "GBM") %>% 
+  bind_rows(df2)
+
+ggplot(df3, aes(x = Proportion, y = Lambda)) +
+  geom_smooth(aes(fill = Model,color = Model), alpha = 0.5) + 
+  facet_wrap(~Protein) +
+  theme_bw()
+
+##########################################
+#############Version 2####################
+##########################################
+
+library(tidyverse)
+library(caret)
+library(gbm)
+library(MuMIn)
+## Read in and generate climatic means
+
+df <- read_rds("df2_corrected_prot_up_low_bound_small.rds") %>% 
+  dplyr::select("TempLag14mean", "rhLag14mean", "uvLag14max", "propOld", "lambda7") %>% 
+  summarise_all(mean)
+
+## Read in and join with Simulation of prots
+
+Prots <- read_rds("Simulation_response_probs.rds") %>% 
+  rename(Est_1A = prot_1A, Est_1B = prot_1B, Est_2A = prot_2A, Est_2B = prot_2B, Est_2C = prot_2C)
+
+df <- bind_cols(df, Prots)
+
+## PRedict GLM
+
+df2 <- df
+
+GLM_Mod <- read_rds("avgmod.95p2.rds")
+
+df2$Lambda <- predict(GLM_Mod, newdata = df)
+
+df2 <- df2 %>% 
+  pivot_longer(starts_with("Est"), names_to = "Protein", values_to = "Proportion") %>% 
+  mutate(Protein = str_remove_all(Protein, "Est_")) %>% 
+  dplyr::select(Protein, Proportion, Lambda) %>% 
+  mutate(Model = "GLM")
+
+## Graph for glm
+## Noisy dynamics
+
+##########################################################
+####GBM##################################################
+##########################################################
+
+df3 <- df
+
+DF <-  read_rds("DF_Prop_gbm_Est.rds") %>% 
+  mutate(ID = 1:n(),Weight = 1/(R2 - R2_Post)^3, Total = sum(Weight), Std_Weight = Weight/Total)
+
+Models <- read_rds("models_Prop_gbm_Est.rds") 
+
+df3$Lambda<- Models %>% 
+  purrr::map(~predict(.x, df3)) %>% 
+  purrr::map2(.y = DF$Std_Weight,~.x*.y) %>% 
+  reduce(`+`)
+
+df3 <-  df3 %>% 
+  pivot_longer(starts_with("Est"), names_to = "Protein", values_to = "Proportion") %>% 
+  mutate(Protein = str_remove_all(Protein, "Est_"))  %>% 
+  dplyr::select(Protein, Proportion, Lambda) %>% 
+  mutate(Model = "GBM") %>% 
+  bind_rows(df2)
+
+ggplot(df3, aes(x = Proportion, y = Lambda)) +
+  geom_smooth(aes(fill = Model,color = Model), alpha = 0.5) + 
+  facet_wrap(~Protein) +
+  theme_bw()
